db62ae800af408f030bf8465b8a88ef8064e9b1b
qf05 <Qf159357>
2019-02-08 23:29:50 +0300

    initial_23_merge_to_x

--- app/src/main/java/ru/javaops/android/tamagotchi/MainActivity.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/MainActivity.java
@@ -1,6 +1,6 @@
 package ru.javaops.android.tamagotchi;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
